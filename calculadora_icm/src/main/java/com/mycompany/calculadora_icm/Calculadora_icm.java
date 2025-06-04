b0cbffcbae87ad281ba6477a7e2001c43fa595bc
jonathan.a.m360@gmail.com <jonathan.a.m360@2gmail.com>
2025-06-04 00:11:17 -0300

    subio tp

---
+++ calculadora_icm/src/main/java/com/mycompany/calculadora_icm/Calculadora_icm.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.calculadora_icm;
+
+
+public class Calculadora_icm {
+
+    public static void main(String[] args) {
+     Pantalla panta = new Pantalla();
+     panta.setVisible(true);
+     panta.setLocationRelativeTo(null);
+     
+        
+    }
+}
